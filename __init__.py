1296a966a10e44637c58694a53ffdc1e0d197b58
Ricardo Colombani <ricardo.colombani@gmail.com>
2025-10-12 14:25:47 -0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .toolkitbrazil import rngCNPJ, rngCNPJfiliais, rngCPF, valCNPJ, valCPF, valCPFuf, ufCapital, ufCPF, ufDDD, strClean
+
+__all__ = ['rngCNPJ', 'rngCNPJfiliais', 'rngCPF', 'valCNPJ',
+           'valCPF', 'valCPFuf', 'ufCapital', 'ufCPF', 'ufDDD', 'strClean']
